f0b81154e5f92b962591cc9c96ae03b119cd530e
Harvey Wang <Harvey@Harveys-MacBook-Pro.local>
2017-01-20 22:38:06 -0400

    jump

---
+++ Assets/script.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour {
+	
+	bool isKeyUp;
+
+	// Use this for initialization
+	void Start () {
+		isKeyUp = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+			GetComponent<Rigidbody2D> ().AddForce (transform.up * 300);
+	}
+}
