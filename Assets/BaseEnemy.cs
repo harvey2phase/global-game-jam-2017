7d39c22663727748e1f7ba20fe5fb071fa984945
Matt Rafuse <mrafuse1997@gmail.com>
2017-01-21 08:13:39 -0400

    Fix to Enemy interface -> BaseEnemy

---
+++ Assets/BaseEnemy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseEnemy : MonoBehaviour {
+
+    public abstract void Move();
+
+    public abstract void Attack();
+
+    public abstract void PlayerInRange();
+}
--- Assets/jump.cs
+++ Assets/jump.cs
@@ -2,48 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class jump : MonoBehaviour {
-	
-	bool isKeyLeft, isKeyRight, grounded, jumped;
+public class Jump : MonoBehaviour {
+    
+    bool isKeyLeft, isKeyRight, grounded, jumped;
 
-	// Use this for initialization
-	void Start () {
-		isKeyLeft = false;
-		isKeyRight = false;
-		grounded = true;
-		jumped = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start () {
+        isKeyLeft = false;
+        isKeyRight = false;
+        grounded = true;
+        jumped = false;
+    }
+    
+    // Update is called once per frame
+    void Update () {
 
-		// grounded?
-		if(GetComponent<Rigidbody2D>().velocity.y == 0)
-			grounded = true;
-		if(GetComponent<Rigidbody2D>().velocity.y != 0)
-			grounded = false;
+        // grounded?
+        if(GetComponent<Rigidbody2D>().velocity.y == 0)
+            grounded = true;
+        if(GetComponent<Rigidbody2D>().velocity.y != 0)
+            grounded = false;
 
-		float Y = GetComponent<Rigidbody2D> ().velocity.y;
+        float Y = GetComponent<Rigidbody2D> ().velocity.y;
 
-		// jump
-		if (Input.GetKeyDown (KeyCode.UpArrow) && Y < 0.000000001) {
-			GetComponent<Rigidbody2D> ().AddForce (transform.up * 2000);
-		}
-		
-		// move left
-		if (Input.GetKeyDown (KeyCode.LeftArrow))
-			isKeyLeft = true;
-		if (Input.GetKeyUp (KeyCode.LeftArrow))
-			isKeyLeft = false;
-		if (isKeyLeft == true)
-			this.gameObject.transform.Translate (-(float).25, 0, 0);
+        // jump
+        if (Input.GetKeyDown (KeyCode.UpArrow) && Y < 0.000000001) {
+            GetComponent<Rigidbody2D> ().AddForce (transform.up * 2000);
+        }
+        
+        // move left
+        if (Input.GetKeyDown (KeyCode.LeftArrow))
+            isKeyLeft = true;
+        if (Input.GetKeyUp (KeyCode.LeftArrow))
+            isKeyLeft = false;
+        if (isKeyLeft == true)
+            this.gameObject.transform.Translate (-(float).25, 0, 0);
 
-		// move right
-		if (Input.GetKeyDown (KeyCode.RightArrow))
-			isKeyRight = true;
-		if (Input.GetKeyUp (KeyCode.RightArrow))
-			isKeyRight = false;
-		if (isKeyRight == true)
-			this.gameObject.transform.Translate ((float).25, 0, 0);
-	}
+        // move right
+        if (Input.GetKeyDown (KeyCode.RightArrow))
+            isKeyRight = true;
+        if (Input.GetKeyUp (KeyCode.RightArrow))
+            isKeyRight = false;
+        if (isKeyRight == true)
+            this.gameObject.transform.Translate ((float).25, 0, 0);
+    }
 }
