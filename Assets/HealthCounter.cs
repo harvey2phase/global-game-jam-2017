c6b261c83aa6a279a02390b50a6abc6a9b5f076c
Tyler Robinson <Tyler Robinson>
2017-01-21 07:56:52 -0400

    Added Health Class to player, modified UI slightly

--- Assets/HealthCounter.cs
+++ Assets/HealthCounter.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthCounter : MonoBehaviour {
-    public int health = 100;
+    public Health health;
 
     Vector2 defaultAnchorMin;
     Vector2 defaultAnchorMax;
@@ -20,7 +20,7 @@ public class HealthCounter : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         Vector2 newVector = new Vector2();
-        newVector.x = defaultAnchorMax.x - (defaultAnchorMax.x - defaultAnchorMin.x) * (health) / 100;
+        newVector.x = defaultAnchorMax.x - (defaultAnchorMax.x - defaultAnchorMin.x) * (health.health) / 100;
         newVector.y = defaultAnchorMin.y;
         rect.anchorMin = newVector;
     }
