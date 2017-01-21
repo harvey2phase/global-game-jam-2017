a5f3993bc4adf83b4f2870318fbdabee418126fa
connor <ctannahill@live.ca>
2017-01-21 08:08:27 -0400

    Enemy Interface

---
+++ Assets/EnemyInterface.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface EnemyInterface : MonoBehaviour {
+
+	public void Move()
+    {
+
+    }
+
+    public void Attack()
+    {
+
+    }
+
+    public void PlayerInRange()
+    {
+
+    }
+}
