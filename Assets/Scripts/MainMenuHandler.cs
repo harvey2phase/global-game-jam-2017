59eee8380d55dd89923e4fcfdc2e6f7f0d731f46
Tyler Robinson <robinsontyler7@gmail.com>
2017-01-22 10:37:55 -0400

    Added base title screen

--- Assets/Scripts/MainMenuHandler.cs
+++ Assets/Scripts/MainMenuHandler.cs
@@ -1,17 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenuHandler : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        SceneManager.LoadScene("OtherSceneName", LoadSceneMode.Single);
+    void Start()
+    {
+        Button btn = GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    private void TaskOnClick()
+    {
+        print("Buton Clicked!");
+        
+        //int num = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+        
     }
 }
