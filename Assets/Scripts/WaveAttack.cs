d219bed60e8a5eb60a1d73d73406668682ca5715
connor <ctannahill@live.ca>
2017-01-22 12:48:05 -0400

    Waveattack cooldown

--- Assets/Scripts/WaveAttack.cs
+++ Assets/Scripts/WaveAttack.cs
@@ -5,6 +5,16 @@ using UnityEngine.UI;
 
 public class WaveAttack : MonoBehaviour {
 
+    public float cooldownTime
+    {
+        get;
+        set;
+    }
+    public float time;
+    public float timeHeld;
+    public bool hasBeenPressed;
+    public bool onCoolDown;
+    
     public SpriteRenderer circle;
     public bool IsFiring
     {
@@ -23,7 +33,12 @@ public class WaveAttack : MonoBehaviour {
     // Use this for initialization
     void Start () {
         IsFiring = false;
+        hasBeenPressed = false;
+        onCoolDown = false;
         PositionOnFire = transform.position;
+        cooldownTime = 3;
+        time = 0;
+        timeHeld = 0;
 
         circle.transform.localScale = new Vector3(Range / 10, Range / 10, Range);
         circle.gameObject.SetActive(false);
@@ -31,14 +46,28 @@ public class WaveAttack : MonoBehaviour {
     
     // Update is called once per frame
     void Update () {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && !onCoolDown && timeHeld < 3)
         {
+            hasBeenPressed = true;
+            timeHeld += Time.deltaTime;
             circle.gameObject.SetActive(true);
             IsFiring = true;
             PositionOnFire = transform.position;
         }
         else
         {
+            if (hasBeenPressed)
+            {
+                time += Time.deltaTime;
+                onCoolDown = true;
+            }
+            if (time > cooldownTime)
+            {
+                hasBeenPressed = false;
+                onCoolDown = false;
+                time = 0;
+                timeHeld = 0;
+            }
             circle.gameObject.SetActive(false);
             IsFiring = false;
         }
